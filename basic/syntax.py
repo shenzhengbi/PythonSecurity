471faafb39e297919311cdb86f19194b86ffaf80
Teacher Egg <TeacherEgg@regg.com>
2023-08-28 10:51:20 +0800

    基础语法

--- basic/syntax.py
+++ basic/syntax.py
@@ -7,3 +7,17 @@
 # a=(1,2,5)
 student={'name':'张三','age':'12'}
 print(student['age'])
+
+string = "这是电话号码"
+#phone1 = input("输入电话号码：") 命令行输入
+phone = 1381119082
+#print(string+phone).这样要转换格式
+print(string,phone)
+print(string + str(phone))
+print('%s%d'%(string,phone))
+print(f"{string}{phone}")
+print("{}{}".format(string,phone))
+
+point = 1234.3223
+print("%.2f"%point)
+print("{:.2f}".format(point))
